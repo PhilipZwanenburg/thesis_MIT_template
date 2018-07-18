2f085994f275584f26dfd227c1e7984859871cca
Philip <philip@McGillMac.local>
2018-07-18 14:05:31 -0400

    Finished the introduction. Minor modifications to be made regarding the DPG contributions when closer to the submission date.

--- introduction/auto/background.el
+++ introduction/auto/background.el
@@ -1,6 +1,10 @@
 (TeX-add-style-hook
  "background"
  (lambda ()
+   (TeX-run-style-hooks
+    "introduction/background_computational_complexity"
+    "introduction/background_complex_geometry"
+    "introduction/background_petrov_galerkin")
    (LaTeX-add-labels
     "sec:intro_background"))
  :latex)
